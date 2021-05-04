66588284d4c7df17878b5a59f43c0a6edaffd9d3
IanMercadal <imercadal@cifpfbmoll.eu>
2021-05-04 11:15:36 +0200

    feat:

---
+++ practica2/src/main/java/edu/pingpong/practica2/stringReverse.java
@@ -0,0 +1,25 @@
+package edu.pingpong.practica2;
+import java.util.Scanner;
+
+public class stringReverse{
+    public void stringInput(){
+        Scanner entrada = new Scanner(System.in);
+        String palabra;
+
+        System.out.print("Introduce la palabra: ");
+        palabra = entrada.nextLine();
+
+        char [] letras = new char[palabra.length()];
+        int letrasIndex = 0;
+        for(int i = palabra.length() -1;i>=0; i--){
+            letras[letrasIndex] = palabra.charAt(i);
+            letrasIndex++;
+        }  
+        String reverse ="";{
+            for (int i=0; i< palabra.length(); i++){
+                reverse += letras[i];
+        System.out.println("La palabra inversa: " + reverse);
+        }
+    }
+}
+}
---
+++ practica2/src/test/java/edu/pingpong/practica2/fiveOrderedTest.java
@@ -0,0 +1,23 @@
+// package edu.pingpong.practica2;
+
+// import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Test;
+
+// /**
+//  * Unit test for simple App.
+//  */
+// public class fiveOrderedTest 
+// {
+//     /**
+//      * Rigorous Test :-)
+//      */
+//     @Test
+//     public void fiveOrderedTest()
+//     {
+//         fiveOrderedNumbers fiveOrderedNumbers = new fiveOrderedNumbers();
+//         float[] arr = new float [5];
+//         assertEquals(arr, fiveOrderedNumbers.five() );
+//     }
+// }
